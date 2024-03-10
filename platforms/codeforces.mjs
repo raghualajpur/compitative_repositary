92eda3d549740a5752d56b3e9304102af9023278
raghualajpur <2010030005@klh.edu.in>
2024-03-11 01:45:46 +0530

    codeforces

---
+++ platforms/codeforces.mjs
@@ -0,0 +1,42 @@
+import fetch from 'node-fetch';
+import cheerio from 'cheerio';
+
+
+const getDetails= async (html1,html2)=>{
+    const codeForcesDetails={}
+    let $ = cheerio.load(html1);
+    let listOfNumbers=[]
+    $('div._UserActivityFrame_counterValue').each(function(){
+        listOfNumbers.push($(this).text().trim().slice(0,-1))
+        
+    })
+    codeForcesDetails["problemsSolved"]=listOfNumbers[0].split(" ")[0]
+    listOfNumbers=[]
+    $('div.info ul li span.user-gray').each(function(){
+        listOfNumbers.push($(this).text())
+    })
+    codeForcesDetails["rating"]=listOfNumbers[0]
+    codeForcesDetails["highestRating"]=listOfNumbers[2]
+    $ = cheerio.load(html2)
+    listOfNumbers=[]
+    $('table.user-contests-table tbody tr td').each(function(){
+        listOfNumbers.push($(this).text())
+    })
+    codeForcesDetails['contestAttended']=listOfNumbers[0]
+
+    return codeForcesDetails
+}
+
+const codeForces= async (url1,url2)=>{
+    const response1 = await fetch(url1);
+    const response2 = await fetch('https://codeforces.com/contests/with/raghualajpur2010030005')
+    const htmlCode1 = await response1.text();
+    const htmlCode2 = await response2.text();
+    const details=await getDetails(htmlCode1,htmlCode2)
+    return details
+}
+
+export default codeForces;
+
+//contest-rating:- text-label-1.dark\\:text-dark-label-1.flex.items-center.text-2xl
+//name:- text-label-1.dark\\:text-dark-label-1.break-all.text-base.font-semibold
--- server.mjs
+++ server.mjs
@@ -1,6 +1,7 @@
 import express from 'express'
 import leetcode from './platforms/leetcode.mjs';
 import codechef from './platforms/codechef.mjs';
+import codeForces from './platforms/codeforces.mjs';
 import cors from 'cors';
 const app=express()
 app.use(express.json())
@@ -11,6 +12,17 @@ app.listen(3010, () => {
 
 app.get("/", (request,response)=>{
     response.send("this is the root page")
+})
+
+app.post("/codeforces", async (request,response)=>{
+    const {username} = request.body
+    try {
+        const codeForceDetails = await codeForces(`https://codeforces.com/profile/${username}`,`https://codeforces.com/contests/with/${username}`);
+        response.send(codeForceDetails)
+    } catch (error) {
+        console.error(error);
+        response.status(500).json({ error: 'Internal Server Error' });
+    }
 })
 
 app.post("/leetcode",async (request,response)=>{
